4ecf6b5d9084e3e1ad19da4dc6906c74f298ed53
Sy Tran <13009812+sytranvn@users.noreply.github.com>
2023-09-01 18:12:47 +0700

    Add math formula and rss

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Math from "$lib/components/Math.svelte";
+	import "katex/dist/katex.min.css";
+	import formula from "./formula.tex?raw";
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -15,9 +21,7 @@
 	<pre>npm create svelte@latest</pre>
 
 	<p>
-		The page you're looking at is purely static HTML, with no
-		client-side interactivity needed. Because of that, we don't need
-		to load any JavaScript. Try viewing the page's source, or
-		opening the devtools network panel and reloading.
+		Score formula:
+		<Math source={formula} />
 	</p>
 </div>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,53 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'node:url';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { rssPlugin } from 'vite-plugin-rss';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export async function listFiles(dir) {
+	const results = []
+	const files = await fs.readdir(dir, { withFileTypes: true, recursive: true })
+	for await (const file of files) {
+		if (file.isFile()) {
+			results.push(path.join(dir, file.name))
+		} else {
+			results.push(...await listFiles(path.join(dir, file.name)))
+		}
+	}
+
+	return results;
+}
+
+const dir = path.dirname(fileURLToPath(import.meta.url))
+const src = path.join(dir, './src/routes')
+let pages = await listFiles(src)
+pages = await Promise.all(pages
+	.filter(f => path.basename(f) === '+page.svelte')
+	.map(async f => ({
+		name: path.relative(src, f.replace('+page.svelte', '')),
+		pubDate: (await fs.stat(f)).mtime
+	})))
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	const BASE_URL = env.BASE_URL
+	const DOMAIN = "https://sytranvn.dev"
+	const BASE_LINK = `${DOMAIN}${BASE_URL}`
+
+	return {
+		plugins: [
+			sveltekit(),
+			rssPlugin({
+				mode: "define",
+				channel: {
+					title: "Sy Tran - Math",
+					link: BASE_LINK,
+					lastBuildDate: new Date(),
+					description: `Recent change on Sy Tran - Math`
+				},
+				items: pages.map(page => ({ link: `${BASE_LINK}/${page.name}`, pubDate: page.pubDate })),
+			})]
+	}
+})
+
