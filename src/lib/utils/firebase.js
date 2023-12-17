a4f26e6fcc6bcbf1dce8b9b5af6dd4c02d679b9c
Sy Tran <13009812+sytranvn@users.noreply.github.com>
2023-12-17 20:23:45 +0700

    chore: update firebase project

--- src/lib/utils/firebase.js
+++ src/lib/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNfeSaDPaO-RwlkK2fSTzedXwLgUKVTCw",
-  authDomain: "diemtinchi.firebaseapp.com",
-  projectId: "diemtinchi",
-  storageBucket: "diemtinchi.appspot.com",
-  messagingSenderId: "627527722288",
-  appId: "1:627527722288:web:e719cb24743468fdf494f5",
-  measurementId: "G-QJX32PXRRN"
+  apiKey: "AIzaSyBRMVIiHzzmaqru64YKtuXVRrBGH46HlZM",
+  authDomain: "dttx-hcmus.firebaseapp.com",
+  projectId: "dttx-hcmus",
+  storageBucket: "dttx-hcmus.appspot.com",
+  messagingSenderId: "1044867665662",
+  appId: "1:1044867665662:web:887ed07b6ec9831f085faf",
+  measurementId: "G-ET0XKV3L7J"
 };
 // Initialize Firebase
 export function initialize() {
